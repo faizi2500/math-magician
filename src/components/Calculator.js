438cbd62169216df51910301d68344f2e872f0b7
Faizan Zahid <faizan.zahid2500@gmail.com>
2021-09-15 14:46:21 +0500

    Add styling for the calculator similar to wireframe

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,30 +13,32 @@ class Calculator extends React.Component {
     return (
       <div className="calculator">
         <div className="answer">
-          <span className="show-answer">
+          <h3 className="show-answer">
             { result }
-          </span>
+          </h3>
         </div>
         <div className="calc-panel">
           <button type="button" className="input">AC</button>
           <button type="button" className="input">+/-</button>
           <button type="button" className="input">%</button>
-          <button type="button" className="input">÷</button>
+          <button type="button" className="input orange-btn">÷</button>
           <button type="button" className="input">7</button>
           <button type="button" className="input">8</button>
           <button type="button" className="input">9</button>
-          <button type="button" className="input">x</button>
+          <button type="button" className="input orange-btn">x</button>
           <button type="button" className="input">4</button>
           <button type="button" className="input">5</button>
           <button type="button" className="input">6</button>
-          <button type="button" className="input">-</button>
+          <button type="button" className="input orange-btn">-</button>
           <button type="button" className="input">1</button>
           <button type="button" className="input">2</button>
           <button type="button" className="input">3</button>
-          <button type="button" className="input">+</button>
+          <button type="button" className="input orange-btn">+</button>
+        </div>
+        <div className="last-row">
           <button type="button" className="input">0</button>
           <button type="button" className="input">.</button>
-          <button type="button" className="input">=</button>
+          <button type="button" className="input orange-btn">=</button>
         </div>
       </div>
     );
